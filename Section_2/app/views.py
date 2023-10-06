a465e4bb373b1b98d20851cc7a2bba736a030233
samrussell-code <sc22s3r@leeds.ac.uk>
2023-10-06 16:49:29 +0100

    section3 and mac venv fix

--- Section_2/app/views.py
+++ Section_2/app/views.py
@@ -1,4 +1,14 @@
+from flask import render_template
 from app import app
+
 @app.route('/')
 def index():
-    return "Hello World!!!"
+    user = {'name': 'Sam Wilson'}
+    return render_template('index.html',
+                           title='Simple template example',
+                           user=user)
+
+@app.route('/fruit') # web path after the root
+def displayFruit():
+    fruits = ["Apple", "Banana", "Orange", "Kiwi"]
+    return render_template("fruit.html",fruits=fruits)
