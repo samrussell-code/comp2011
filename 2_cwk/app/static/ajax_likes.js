708cab8fbc79f60ffd460d52144a659c18eedbe2
samrussell-code <sc22s3r@leeds.ac.uk>
2023-11-25 20:21:47 +0000

    like icon functioning

--- 2_cwk/app/static/ajax_likes.js
+++ 2_cwk/app/static/ajax_likes.js
@@ -5,14 +5,27 @@ $(document).ready(function () {
         var likesCountSpan = $(this).siblings('.likes-count');
         var responseLocation = $('#response-' + movieId);
 
+        // Check if the like button is already toggled as liked
+        var isLiked = $(this).hasClass('liked');
+
         // Simulate an AJAX request (replace this with your actual AJAX request)
         // For this example, we're updating the likes count and response location immediately
         var currentLikes = parseInt(likesCountSpan.text());
-        likesCountSpan.text(currentLikes + 1);
-        responseLocation.text("");
 
-        // Toggle the like button icon (add your logic here based on actual server response)
+        if (isLiked) {
+            // If already liked, decrement the counter and update the response
+            likesCountSpan.text(currentLikes - 1);
+            // responseLocation.text("Unliked!");
+        } else {
+            // If not liked, increment the counter and update the response
+            likesCountSpan.text(currentLikes + 1);
+            // responseLocation.text("Liked!");
+        }
+
+        // Toggle the like button icon and update the image source
         $(this).toggleClass('liked');
+        var newImageSrc = isLiked ? 'likes_ico.png' : 'likes_ico_liked.png';
+        $(this).attr('src', 'static/' + newImageSrc);
     }
 
     // Initial setup for like button click event on existing movie cards
@@ -20,7 +33,6 @@ $(document).ready(function () {
 
     // Set up an event listener for newly loaded movie cards
     $(document).on('DOMNodeInserted', function (e) {
-        // console.log($(e.target).hasClass('container'))
         if ($(e.target).hasClass('container')) {
             // Newly added movie card, attach the like button click event
             $(e.target).find(".like-icon").on("click", handleLikeButtonClick);
